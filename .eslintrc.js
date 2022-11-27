86c1f36f9e92b1d7d2981897f66326c0f09395b6
Konstantin Moskvin <mr.koctia@gmail.com>
2022-11-27 23:28:25 +0300

    task 01

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,48 +1,20 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es6": true,
-        "node": true
-    },
-    "extends": [
-        "eslint:recommended",
-        "plugin:react/recommended"
-    ],
-    "globals": {
-        "Atomics": "readonly",
-        "SharedArrayBuffer": "readonly"
-    },
-    "parserOptions": {
-        "ecmaFeatures": {
-            "jsx": true
-        },
-        "ecmaVersion": 2018,
-        "sourceType": "module"
-    },
-    "plugins": [
-        "react"
-    ],
-    "rules": {
-    }
+  env: {
+    commonjs: true,
+    es6: true,
+    node: true,
+    mocha: true,
+  },
+  extends: [
+    'airbnb-base',
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parserOptions: {
+    ecmaVersion: 2018,
+  },
+  rules: {
+  },
 };
-
-// module.exports = {
-//   env: {
-//     commonjs: true,
-//     es6: true,
-//     node: true,
-//     mocha: true,
-//   },
-//   extends: [
-//     'airbnb-base',
-//   ],
-//   globals: {
-//     Atomics: 'readonly',
-//     SharedArrayBuffer: 'readonly',
-//   },
-//   parserOptions: {
-//     ecmaVersion: 2018,
-//   },
-//   rules: {
-//   },
-// };
--- src/01-strings-tasks.js
+++ src/01-strings-tasks.js
@@ -66,7 +66,7 @@ function getStringFromTemplate(firstName, lastName) {
  *   'Hello, Chuck Norris!' => 'Chuck Norris'
  */
 function extractNameFromTemplate(value) {
-  return value.split(',')[1].replace('!','').trim();
+  return value.split(',')[1].replace('!', '').trim();
 }
 
 
