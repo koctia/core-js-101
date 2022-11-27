2a12d2dcb8845455f2f70b83acec59d17991339a
Konstantin Moskvin <mr.koctia@gmail.com>
2022-11-27 23:16:40 +0300

    task 01

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,20 +1,48 @@
 module.exports = {
-  env: {
-    commonjs: true,
-    es6: true,
-    node: true,
-    mocha: true,
-  },
-  extends: [
-    'airbnb-base',
-  ],
-  globals: {
-    Atomics: 'readonly',
-    SharedArrayBuffer: 'readonly',
-  },
-  parserOptions: {
-    ecmaVersion: 2018,
-  },
-  rules: {
-  },
+    "env": {
+        "browser": true,
+        "es6": true,
+        "node": true
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:react/recommended"
+    ],
+    "globals": {
+        "Atomics": "readonly",
+        "SharedArrayBuffer": "readonly"
+    },
+    "parserOptions": {
+        "ecmaFeatures": {
+            "jsx": true
+        },
+        "ecmaVersion": 2018,
+        "sourceType": "module"
+    },
+    "plugins": [
+        "react"
+    ],
+    "rules": {
+    }
 };
+
+// module.exports = {
+//   env: {
+//     commonjs: true,
+//     es6: true,
+//     node: true,
+//     mocha: true,
+//   },
+//   extends: [
+//     'airbnb-base',
+//   ],
+//   globals: {
+//     Atomics: 'readonly',
+//     SharedArrayBuffer: 'readonly',
+//   },
+//   parserOptions: {
+//     ecmaVersion: 2018,
+//   },
+//   rules: {
+//   },
+// };
