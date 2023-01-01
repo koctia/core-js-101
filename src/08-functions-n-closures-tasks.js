7dad38dce0d25e4794c8eb0099ac6e093ab3cc6f
Konstantin Moskvin <mr.koctia@gmail.com>
2023-01-01 03:59:02 +0300

    task 08

--- src/08-functions-n-closures-tasks.js
+++ src/08-functions-n-closures-tasks.js
@@ -86,8 +86,9 @@ function getPolynom(...arr) {
  *   ...
  *   memoizer() => the same random number  (next run, returns the previous cached result)
  */
-function memoize(/* func */) {
-  throw new Error('Not implemented');
+function memoize(func) {
+  const res = func();
+  return () => res;
 }
 
 
@@ -106,8 +107,17 @@ function memoize(/* func */) {
  * }, 2);
  * retryer() => 2
  */
-function retry(/* func, attempts */) {
-  throw new Error('Not implemented');
+function retry(func, attempts) {
+  return () => {
+    for (let i = 0; i < attempts; i += 1) {
+      try {
+        return func();
+      } catch (err) {
+        console.log(err);
+      }
+    }
+    return false;
+  };
 }
 
 
@@ -134,8 +144,14 @@ function retry(/* func, attempts */) {
  * cos(3.141592653589793) ends
  *
  */
-function logger(/* func, logFunc */) {
-  throw new Error('Not implemented');
+function logger(func, logFunc) {
+  return (...args) => {
+    const arr = JSON.stringify(args);
+    logFunc(`${func.name}(${arr.slice(1, arr.length - 1)}) starts`);
+    const current = func(...args);
+    logFunc(`${func.name}(${arr.slice(1, arr.length - 1)}) ends`);
+    return current;
+  };
 }
 
 
@@ -152,8 +168,8 @@ function logger(/* func, logFunc */) {
  *   partialUsingArguments(fn, 'a','b','c')('d') => 'abcd'
  *   partialUsingArguments(fn, 'a','b','c','d')() => 'abcd'
  */
-function partialUsingArguments(/* fn, ...args1 */) {
-  throw new Error('Not implemented');
+function partialUsingArguments(fn, ...args1) {
+  return (...x) => fn(...args1, ...x);
 }
 
 
@@ -174,8 +190,13 @@ function partialUsingArguments(/* fn, ...args1 */) {
  *   getId4() => 7
  *   getId10() => 11
  */
-function getIdGeneratorFunction(/* startFrom */) {
-  throw new Error('Not implemented');
+function getIdGeneratorFunction(startFrom) {
+  let counter = startFrom;
+  counter -= 1;
+  return () => {
+    counter += 1;
+    return counter;
+  };
 }
 
 
